18d06d863d2cae412d5c1e64601e39663d4534d1
TheTaylorLee <53202926+TheTaylorLee@users.noreply.github.com>
2020-08-31 08:43:17 -0500

    Update Microsoft.PowerShell_profile.ps1

--- PS7-x64/Microsoft.PowerShell_profile.ps1
+++ PS7-x64/Microsoft.PowerShell_profile.ps1
@@ -3,9 +3,9 @@
 #Powershell Customization
 function prompt {
     $location = Get-Location
-    Write-Host -NoNewLine "$(HOSTNAME.EXE) "                  -ForegroundColor Green
-    Write-Host -NoNewLine '~'                                 -ForegroundColor Yellow
-    Write-Host -NoNewLine $(Get-Location).Path.Split('\')[-1] -ForegroundColor Cyan
+    Write-Host -NoNewline "$(HOSTNAME.EXE) "                  -ForegroundColor Green
+    Write-Host -NoNewline '~'                                 -ForegroundColor Yellow
+    Write-Host -NoNewline $(Get-Location).Path.Split('\')[-1] -ForegroundColor Cyan
     Write-Host -NoNewline ">" -ForegroundColor Green
 
     $Adminp = [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")
@@ -22,6 +22,50 @@ Set-PSReadLineKeyHandler -Chord Ctrl+Space -Function MenuComplete
 Set-PSReadLineOption -BellStyle None
 Set-PSReadLineKeyHandler -Chord Tab -Function MenuComplete
 #Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+
+Function Set-WindowSize {
+    #Specify Window Size, Buffer, and Histor Parameters
+    [int]$Height = 30
+    [int]$Width = 140
+    [int]$Buffer = 8000
+
+    $console = $host.ui.rawui
+    $ConBuffer = $console.BufferSize
+    $ConSize = $console.WindowSize
+
+    $currWidth = $ConSize.Width
+    $currHeight = $ConSize.Height
+
+    # if height is too large, set to max allowed size
+    if ($Height -gt $host.UI.RawUI.MaxPhysicalWindowSize.Height) {
+        $Height = $host.UI.RawUI.MaxPhysicalWindowSize.Height
+    }
+
+    # if width is too large, set to max allowed size
+    if ($Width -gt $host.UI.RawUI.MaxPhysicalWindowSize.Width) {
+        $Width = $host.UI.RawUI.MaxPhysicalWindowSize.Width
+    }
+
+    # If the Buffer is wider than the new console setting, first reduce the width
+    If ($ConBuffer.Width -gt $Width ) {
+        $currWidth = $Width
+    }
+
+    # If the Buffer is higher than the new console setting, first reduce the height
+    If ($ConBuffer.Height -gt $Height ) {
+        $currHeight = $Height
+    }
+
+    # initial resizing if needed
+    $host.UI.RawUI.WindowSize = New-Object System.Management.Automation.Host.size($currWidth, $currHeight)
+
+    # Set the Buffer
+    $host.UI.RawUI.BufferSize = New-Object System.Management.Automation.Host.size($Width, $Buffer)
+
+    # Now set the WindowSize
+    $host.UI.RawUI.WindowSize = New-Object System.Management.Automation.Host.size($Width, $Height)
+}
+Set-WindowSize
 #-------------------------------------------------------------------------------------------------------------------------------------------------------
 
 #Downloads folder variable
@@ -36,3 +80,4 @@ if (Test-Path($ChocolateyProfile)) {
 
 $ErrorActionPreference = 'Continue'
 Import-Module AdminToolbox
+Import-Module BetterCredentials
