71c281131fd562e84a641948611c22b3861fcbd8
Admin Taylor Lee <Admin-Taylor@visualutions.com>
2021-02-26 12:56:19 -0600

    update

--- PS7-x64/Modules/AdminToolbox.Exchange/Public/Get-VirtualDirectories.ps1
+++ PS7-x64/Modules/AdminToolbox.Exchange/Public/Get-VirtualDirectories.ps1
@@ -19,20 +19,20 @@ Function Get-VirtualDirectories {
         [Parameter(Mandatory = $true)]$server
     )
 
-    Write-Host "Powershell Virtual Directories"   -Foregroundcolor Green
+    Write-Host "Powershell Virtual Directories"   -ForegroundColor Green
     Get-PowerShellVirtualDirectory -server $server | Format-List internalurl, externalurl
-    Write-Host "Active-Sync Virtual Directories"  -Foregroundcolor Green
+    Write-Host "Active-Sync Virtual Directories"  -ForegroundColor Green
     Get-ActiveSyncVirtualDirectory -server $server | Format-List internalurl, externalurl
-    Write-Host "Web Services Virtual Directories"    -Foregroundcolor Green
+    Write-Host "Web Services Virtual Directories"    -ForegroundColor Green
     Get-WebServicesVirtualDirectory -server $server | Format-List internalurl, externalurl
-    Write-Host "OWA Virtual Directories"    -Foregroundcolor Green
+    Write-Host "OWA Virtual Directories"    -ForegroundColor Green
     Get-OwaVirtualDirectory -server $server | Format-List internalurl, externalurl
-    Write-Host "AutoDiscover Virtual Directories"   -Foregroundcolor Green
+    Write-Host "AutoDiscover Virtual Directories"   -ForegroundColor Green
     Get-AutodiscoverVirtualDirectory -server $server | Format-List internalurl, externalurl
-    Write-Host "ECP Virtual Directories"    -Foregroundcolor Green
+    Write-Host "ECP Virtual Directories"    -ForegroundColor Green
     Get-EcpVirtualDirectory -server $server | Format-List internalurl, externalurl
-    Write-Host "OAB Virtual Directories"    -Foregroundcolor Green
+    Write-Host "OAB Virtual Directories"    -ForegroundColor Green
     Get-OABvirtualDirectory -server $server | Format-List internalurl, externalurl
-    Write-Host "MAPI Virtual Directories"    -Foregroundcolor Green
+    Write-Host "MAPI Virtual Directories"    -ForegroundColor Green
     get-mapivirtualdirectory -server $server | Format-List internalurl, externalurl
 }
