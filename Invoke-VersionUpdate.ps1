1e9b5fb61c9b4260e01b127dd22ac8f80ab8e4c1
TheTaylorLee <53202926+TheTaylorLee@users.noreply.github.com>
2021-05-19 12:55:39 -0500

    update

---
+++ Invoke-VersionUpdate.ps1
@@ -0,0 +1,67 @@
+#This Function is for use by the Packaged Microsoft Powershell Profile. It handles version upgrades when called.
+
+Function Deploy-PSPortable {
+
+    try {
+        taskkill /im pwsh.exe /F
+        taskkill /im ConEMU.exe /F
+        taskkill /im ConEMUC64.exe /F
+    }
+    catch {
+    }
+
+    start-sleep -seconds 5
+
+    #Remove old package
+    if (Test-Path $env:ProgramData\PS7x64) {
+        Remove-Item $env:ProgramData\PS7x64 -Recurse -Force
+    }
+
+    #Download new package as zip file
+    Function Invoke-DLPSPortable {
+        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+        $url = "https://github.com/thetaylorlee/psportable/archive/master.zip"
+        $output = "$env:ProgramData\PS7x64.zip"
+        $wc = New-Object System.Net.WebClient
+        $wc.DownloadFile($url, $output)
+    }; Invoke-DLPSPortable
+
+    #Unzip to path download package
+    function Invoke-Unzip2 {
+        [cmdletbinding()]
+        param(
+            [string]$zipfile,
+            [string]$outpath
+        )
+
+
+        if (Get-Command expand-archive -ErrorAction 'SilentlyContinue') {
+            Expand-Archive -Path $zipfile -DestinationPath $outpath
+        }
+
+
+
+        else {
+            try {
+                #Allows for unzipping folders in older versions of powershell if .net 4.5 or newer exists
+                Add-Type -AssemblyName System.IO.Compression.FileSystem
+                [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+            }
+
+            catch {
+                #If .net 4.5 or newer not present, com classes are used. This process is slower.
+                [void] (New-Item -Path $outpath -ItemType Directory -Force)
+                $Shell = New-Object -com Shell.Application
+                $Shell.Namespace($outpath).copyhere($Shell.NameSpace($zipfile).Items(), 4)
+            }
+        }
+    }
+
+    Invoke-Unzip2 -zipfile "$env:ProgramData\PS7x64.zip" -outpath "$env:ProgramData"
+    Rename-Item "$env:ProgramData\PSPortable-master" "$env:ProgramData\PS7x64"
+    Remove-Item "$env:ProgramData\PS7x64.zip" -Force
+
+    #Pin shortcut to taskbar
+    Invoke-Item "$env:ProgramData\PS7x64\PS7-x64\pwsh.exe.lnk"
+    exit
+}; Deploy-PSPortable
--- PS7-x64/Microsoft.PowerShell_profile.ps1
+++ PS7-x64/Microsoft.PowerShell_profile.ps1
@@ -48,11 +48,7 @@ if ($query.Version -le "2.2") {
 }
 
 Function Set-PSReadlineIntellisenseOptions {
-    #Set colors for intellisense prediction
-    Set-PSReadLineOption -Colors @{
-        InlinePrediction = '#85C1E9'
-        ListPrediction   = '#27FF00'
-    }
+
     #Set viewStyle based on powershell version requirements
     if ($psversiontable.psversion.major -ge 7 ) {
         Set-PSReadLineOption -PredictionViewStyle ListView
@@ -114,6 +110,25 @@ Set-PSReadLineKeyHandler -Key F12 `
         [Microsoft.PowerShell.PSConsoleReadLine]::Insert(($command -join "`n"))
     }
 }
+
+#Set Colors
+Set-PSReadLineOption -Colors @{ Command = "`e[97m" }
+Set-PSReadLineOption -Colors @{ Comment = "`e[32m" }
+Set-PSReadLineOption -Colors @{ ContinuationPrompt = "`e[37m" }
+Set-PSReadLineOption -Colors @{ Emphasis = "`e[96m" }
+Set-PSReadLineOption -Colors @{ Error = "`e[91m" }
+Set-PSReadLineOption -Colors @{ Keyword = "`e[92m" }
+Set-PSReadLineOption -Colors @{ ListPredictionSelected = "`e[48;5;238m" }
+Set-PSReadLineOption -Colors @{ Member = "`e[97m" }
+Set-PSReadLineOption -Colors @{ Number = "`e[97m" }
+Set-PSReadLineOption -Colors @{ Operator = "`e[90m" }
+Set-PSReadLineOption -Colors @{ Parameter = "`e[90m" }
+Set-PSReadLineOption -Colors @{ Selection = "`e[30;47m" }
+Set-PSReadLineOption -Colors @{ String = "`e[36m" }
+Set-PSReadLineOption -Colors @{ Type = "`e[37m" }
+Set-PSReadLineOption -Colors @{ Variable = "`e[92m" }
+Set-PSReadLineOption -Colors @{ ListPrediction = "`e[38;2;39;255;0m" }
+Set-PSReadLineOption -Colors @{ InlinePrediction = "`e[38;2;133;193;233m" }
 
 #-------------------------------------------------------------------------------------------------------------------------------------------------------
 
