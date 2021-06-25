42feb9eeea21c2ff569ec1e0a42478d1941f1c4c
TheTaylorLee <53202926+TheTaylorLee@users.noreply.github.com>
2021-06-25 00:52:41 -0500

    Update Profile

--- PS7-x64/Microsoft.PowerShell_profile.ps1
+++ PS7-x64/Microsoft.PowerShell_profile.ps1
@@ -178,16 +178,25 @@ Set-WindowSize
 
 #-------------------------------------------------------------------------------------------------------------------------------------------------------
 
+#Imports the Exchange Online Module if exists
+$CreateEXOPSSession = (Get-ChildItem -Path $Env:LOCALAPPDATA\Apps\2.0* -Filter CreateExoPSSession.ps1 -Recurse -ErrorAction SilentlyContinue -Force | Select-Object -Last 1).DirectoryName
+Import-Module  "$CreateEXOPSSession\CreateExoPSSession.ps1" -Force
+
+#Clear EXO and exchange messages
+Clear-Host
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------
+
 Function Invoke-VersionCheck {
 
     $CurrentVersion = Get-Content "C:\ProgramData\PS7x64\version.txt"
 
-    $VersionCheck = (Invoke-WebRequest https://raw.githubusercontent.com/TheTaylorLee/PSPortable/master/version.txt -Headers @{"Cache-Control" = "no-cache" }).content | Select-String $CurrentVersion
+    $VersionCheck = (Invoke-WebRequest https://raw.githubusercontent.com/TheTaylorLee/PSPortable/master/version.txt -Headers @{"Cache-Control" = "no-cache" } -UseBasicParsing).content | Select-String $CurrentVersion
 
     if ($VersionCheck) {
     }
     else {
-        (Invoke-WebRequest https://raw.githubusercontent.com/TheTaylorLee/PSPortable/master/Changelog.md -Headers @{"Cache-Control" = "no-cache" }).content
+        (Invoke-WebRequest https://raw.githubusercontent.com/TheTaylorLee/PSPortable/master/Changelog.md -Headers @{"Cache-Control" = "no-cache" } -UseBasicParsing).content
         Write-Host " "
 
         Write-Host "Current $CurrentVersion" -ForegroundColor Green
@@ -204,15 +213,6 @@ Function Update-Console {
     Start-Process -FilePath powershell.exe -ArgumentList "-executionpolicy bypass", -noprofile, -NoLogo, "-File $env:ProgramData\PS7x64\Invoke-VersionUpdate.ps1"
 
 }
-
-#-------------------------------------------------------------------------------------------------------------------------------------------------------
-
-#Imports the Exchange Online Module if exists
-$CreateEXOPSSession = (Get-ChildItem -Path $Env:LOCALAPPDATA\Apps\2.0* -Filter CreateExoPSSession.ps1 -Recurse -ErrorAction SilentlyContinue -Force | Select-Object -Last 1).DirectoryName
-Import-Module  "$CreateEXOPSSession\CreateExoPSSession.ps1" -Force
-
-#Clear EXO and exchange messages
-Clear-Host
 
 #-------------------------------------------------------------------------------------------------------------------------------------------------------
 
