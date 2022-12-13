ee3a9e433637a83e834540f37b7642a87b8b3dc1
TheTaylorLee <53202926+TheTaylorLee@users.noreply.github.com>
2022-12-12 23:53:46 -0600

    updated requirements

--- Deploy-PSPortable.ps1
+++ Deploy-PSPortable.ps1
@@ -69,19 +69,22 @@ Function Deploy-PSPortable {
         }
     }
 
-    Install-Module Az -Force -Scope AllUsers
-    Install-Module AzureAD -Force -Scope AllUsers
-    Install-Module AzViz -Force -Scope AllUsers
-    Install-Module BetterCredentials -Force -Scope AllUsers
-    Install-Module ExchangeOnlineManagement -Force -Scope AllUsers
-    Install-Module Microsoft.Graph -Force -Scope AllUsers
-    Install-Module MicrosoftTeams -Force -Scope AllUsers
-    Install-Module MSOnline -Force -Scope AllUsers
-    Install-Module PlatyPS -Force -Scope AllUsers
-    Install-Module PSGraph -Force -Scope AllUsers
-    Install-Module PSTeams -Force -Scope AllUsers
-    Install-Module ReportHTML -Force -Scope AllUsers
-    Install-Module VMWare.Powercli -Force -Scope AllUsers
+    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+    Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201
+
+    Install-Module Az -Force -Scope AllUsers -Confirm:$false
+    Install-Module AzureAD -Force -Scope AllUsers -Confirm:$false
+    Install-Module AzViz -Force -Scope AllUsers -Confirm:$false
+    Install-Module BetterCredentials -Force -Scope AllUsers -Confirm:$false
+    Install-Module ExchangeOnlineManagement -Force -Scope AllUsers -Confirm:$false
+    Install-Module Microsoft.Graph -Force -Scope AllUsers -Confirm:$false
+    Install-Module MicrosoftTeams -Force -Scope AllUsers -Confirm:$false
+    Install-Module MSOnline -Force -Scope AllUsers -Confirm:$false
+    Install-Module PlatyPS -Force -Scope AllUsers -Confirm:$false
+    Install-Module PSGraph -Force -Scope AllUsers -Confirm:$false
+    Install-Module PSTeams -Force -Scope AllUsers -Confirm:$false
+    Install-Module ReportHTML -Force -Scope AllUsers -Confirm:$false
+    Install-Module VMWare.Powercli -Force -Scope AllUsers -Confirm:$false
 
     Invoke-Unzip -zipfile "$env:ProgramData\PS7x64.zip" -outpath "$env:ProgramData"
     Rename-Item "$env:ProgramData\PSPortable-master" "$env:ProgramData\PS7x64"
